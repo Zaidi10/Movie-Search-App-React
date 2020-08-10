3233ef44cb590ea89109768cc7cc1f3b8030c60a
Zaidi10 <16ume051@lnmiit.ac.in>
2020-08-10 12:15:15 +0530

    Updates

---
+++ precache-manifest.7f9c8dd3d32cc1e7cc7c534bdd1587bc.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13bbf5295ab5e0128d09979d072bd96c",
+    "url": "/Movie-Search-App-React/index.html"
+  },
+  {
+    "revision": "7d3c17f9db2099b41078",
+    "url": "/Movie-Search-App-React/static/css/2.eb6b8b82.chunk.css"
+  },
+  {
+    "revision": "6cdad5e859d5d17a379c",
+    "url": "/Movie-Search-App-React/static/css/main.b0acebd6.chunk.css"
+  },
+  {
+    "revision": "7d3c17f9db2099b41078",
+    "url": "/Movie-Search-App-React/static/js/2.bb3fec74.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/Movie-Search-App-React/static/js/2.bb3fec74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cdad5e859d5d17a379c",
+    "url": "/Movie-Search-App-React/static/js/main.f8ad0b0f.chunk.js"
+  },
+  {
+    "revision": "2261af05582d6f0dc8a8",
+    "url": "/Movie-Search-App-React/static/js/runtime-main.c92e3b74.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Movie-Search-App-React/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Movie-Search-App-React/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Movie-Search-App-React/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Movie-Search-App-React/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Movie-Search-App-React/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/Movie-Search-App-React/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/Movie-Search-App-React/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/Movie-Search-App-React/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/Movie-Search-App-React/static/media/notification.c0d3c94c.eot"
+  }
+]);
